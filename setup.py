6b5d4795a68896546aae89092d8e18b22ba5fea9
fghdxfvdxfvdf <138147183+fghdxfvdxfvdf@users.noreply.github.com>
2023-09-05 09:39:34 +0300

    Add files via upload
    модуль №7 завдання

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='usefull',
+    version='0.1',
+    description='Сортування папок',
+    url="https://github.com/fghdxfvdxfvdf/DZ_7_2",
+    author="Максим Гребеніченко",
+    author_email="",
+    license="MIT",
+    packages=["clean_folder"],
+    install_requires=[
+        'markdown'
+        ],
+    zip_safe=False
+)
